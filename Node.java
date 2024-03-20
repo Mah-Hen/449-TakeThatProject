59974594a80a06e0c8589c0cf7d305acd9f591e9
Mahlaki <mahlaki14@gmail.com>
2024-03-20 12:39:41 -0400

    Day 3

--- Node.java
+++ Node.java
@@ -13,12 +13,67 @@ public Node(Board brd, Node parent) {
     this.parent = parent; // initialize state parent
 }
 
-public String minimaxSearch(Board brd){
-    String valueMove = maxValue(brd);
-    
+public int minimaxSearch(Board brd){
+    String[] valueMoveList = maxValue(brd);
+    int utilValue = Integer.parseInt(valueMoveList[0]);
+    if(valueMoveList[1]!=null){
+        int moveValue = Integer.parseInt(valueMoveList[1]);
+        return moveValue;}
+    return null; // Gotta find a number to act as our null flag or maybe change the return type
 }
 
-private ArrayList action(Board brd){
+public Integer[] maxValue(Board brd){
+    Integer [] maxValueMoveList = new Integer [2];
+    if(isTerminal(brd)){
+        maxValueMoveList[0] = utilityFunction(brd);
+        maxValueMoveList[1] = null;
+        return maxValueMoveList;
+    }
+    int lowValue = Integer.MIN_VALUE;
+    for(Integer action:Actions(brd)){
+        Board newBoard = Result(brd, action);
+        Integer [] minValueMoveList = minValue(newBoard);
+        Integer minLowValue = minValueMoveList[0]; // Utility Function
+        if (minLowValue > lowValue){ // if the node's util function is greater than the low value
+            lowValue = minLowValue;
+            maxValueMoveList[0] = lowValue;
+            maxValueMoveList[1] = action; 
+        }
+    }
+    return maxValueMoveList;
+}
+
+public Integer[] minValue(Board brd){
+    Integer[] minValueMoveList = new Integer[2];
+    if(isTerminal(brd)){
+        minValueMoveList[0] = utilityFunction(brd);
+        minValueMoveList[1] = null;
+        return minValueMoveList;
+    }
+    int highValue = Integer.MAX_VALUE;
+    for(Integer action:Actions(brd)){
+        Board newBoard = Result(brd, action); // new Board after the action is applied
+        Integer[] maxValueMoveList = maxValue(newBoard);
+        Integer maxLowValue = maxValueMoveList[0]; // Utility Function
+        if(maxLowValue < highValue){
+            highValue = maxLowValue;
+            minValueMoveList[0] = highValue;
+            minValueMoveList[1] = action;
+        }
+    } 
+    return minValueMoveList;
+}
+
+private Board Result(Board brd, Integer action) {
+    Board newBoard = new Board(); // Getters and Setters
+    return newBoard;
+}
+
+private Integer utilityFunction(Board brd) {
+    return 0; 
+}
+
+private ArrayList<Integer> Actions(Board brd){
     ArrayList<Integer> actions = new ArrayList<>();
 
     return actions;
